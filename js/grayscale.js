98fdb5c1beac7ccffd92959cf1b157bc01a4b7ae
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-07-24 15:15:34 +0200

    Primary edits

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,173 +1,92 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+/* affix the navbar after scroll below header */
+$('#nav').affix({
+  offset: {
+    top: $('header').height() - $('#nav').height() + 100
+  }
 });
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+/* highlight the top nav as scrolling occurs */
+$('body').scrollspy({ target: '#nav' });
+
+
+/* smooth scrolling for nav sections */
+$('#nav .navbar-nav li>a').click(function () {
+  var link = $(this).attr('href');
+  var posi = $(link).offset().top + 20;
+  $('body,html').animate({scrollTop: posi}, 700);
 });
 
-// Google Maps Scripts
-// When the window has finished loading create our google map below
+
 google.maps.event.addDomListener(window, 'load', init);
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+  var mapOptions = {
+    zoom: 14,
 
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+    center: new google.maps.LatLng(46.187145, 6.140515),
+    disableDefaultUI: true,
+    scrollwheel: false,
+    draggable: false,
 
-        // Disables the default Google Maps UI components
-        disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false,
+    // How you would like to style the map.
+    // This is where you would paste any style found on Snazzy Maps.
+    styles: [
+      {"featureType": "water", "elementType": "geometry", "stylers": [
+        {"color": "#a2daf2"}
+      ]},
+      {"featureType": "landscape.man_made", "elementType": "geometry", "stylers": [
+        {"color": "#f7f1df"}
+      ]},
+      {"featureType": "landscape.natural", "elementType": "geometry", "stylers": [
+        {"color": "#d0e3b4"}
+      ]},
+      {"featureType": "landscape.natural.terrain", "elementType": "geometry", "stylers": [
+        {"visibility": "off"}
+      ]},
+      {"featureType": "poi.park", "elementType": "geometry", "stylers": [
+        {"color": "#bde6ab"}
+      ]},
+      {"featureType": "poi", "elementType": "labels", "stylers": [
+        {"visibility": "off"}
+      ]},
+      {"featureType": "poi.medical", "elementType": "geometry", "stylers": [
+        {"color": "#fbd3da"}
+      ]},
+      {"featureType": "poi.business", "stylers": [
+        {"visibility": "off"}
+      ]},
+      {"featureType": "road", "elementType": "geometry.stroke", "stylers": [
+        {"visibility": "off"}
+      ]},
+      {"featureType": "road", "elementType": "labels", "stylers": [
+        {"visibility": "off"}
+      ]},
+      {"featureType": "road.highway", "elementType": "geometry.fill", "stylers": [
+        {"color": "#ffe15f"}
+      ]},
+      {"featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [
+        {"color": "#efd151"}
+      ]},
+      {"featureType": "road.arterial", "elementType": "geometry.fill", "stylers": [
+        {"color": "#ffffff"}
+      ]},
+      {"featureType": "road.local", "elementType": "geometry.fill", "stylers": [
+        {"color": "black"}
+      ]},
+      {"featureType": "transit.station.airport", "elementType": "geometry.fill", "stylers": [
+        {"color": "#cfb2db"}
+      ]}
+    ]
+  };
 
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "landscape",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 29
-            }, {
-                "weight": 0.2
-            }]
-        }, {
-            "featureType": "road.arterial",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 18
-            }]
-        }, {
-            "featureType": "road.local",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "featureType": "poi",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 21
-            }]
-        }, {
-            "elementType": "labels.text.stroke",
-            "stylers": [{
-                "visibility": "on"
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "elementType": "labels.text.fill",
-            "stylers": [{
-                "saturation": 36
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 40
-            }]
-        }, {
-            "elementType": "labels.icon",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }, {
-            "featureType": "transit",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 19
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }, {
-                "weight": 1.2
-            }]
-        }]
-    };
+  var mapElement = document.getElementById('map');
 
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
+  var map = new google.maps.Map(mapElement, mapOptions);
 
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
-    });
+  var image = 'img/map-marker.png';
+  var myLatLng = new google.maps.LatLng(46.187145, 6.140515);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map
+  });
 }
